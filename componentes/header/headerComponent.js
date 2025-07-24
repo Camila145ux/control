fb73fecf3e251706c5e34bd35df2826da1d6c587
cami <pcbarrondo@gmail.com>
2025-07-24 10:08:01 +0200

    header finalizado

--- componentes/header/headerComponent.js
+++ componentes/header/headerComponent.js
@@ -2,9 +2,15 @@ function header() {
     let fondo = document.createElement("div");
     fondo.className = "rectangulo";
 
+    let nombre = document.createElement("nombre");
+    nombre.innerText = "Camila Barrondo"
+    nombre.className ="nombre";
+
     let circulo = document.createElement("div");
+    circulo.innerText = "🌼"
     circulo.className = "circulo";
 
+    fondo.appendChild(nombre);
     fondo.appendChild(circulo);
 
     return fondo;
