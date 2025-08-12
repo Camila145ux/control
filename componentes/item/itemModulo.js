dccb0486af36a3962a9bc79893f3a6f55e0f9425
cami <pcbarrondo@gmail.com>
2025-08-12 19:16:00 +0200

    titulo HOLA Modificado

--- componentes/item/itemModulo.js
+++ componentes/item/itemModulo.js
@@ -13,22 +13,5 @@ export function item(titulo, urlImg){
         header.classList.add('ocultar');
     });
     //funcion anonima: ()=>{}
-
-    let titulo = document.createElement('h1');
-    titulo.className = "titulo";
-    titulo.innerText = "pochacco";
-
-    let imagen = document.createElement("img");
-    imagen.className = "img";
-    imagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZCkG1nPCC3--9YgvxGqc6c34qv_UpHGnPlQ&s";
-
-    let descripcion = document.createElement("p");
-    descripcion.className = "descrip";
-    descripcion.innerText = "Pochacco es pequeño, curioso y torpe y a veces mete la nariz donde no lo llaman. Vive en el Callejón del Ruiseñor y le encanta el baloncesto.";
-
-    item.appendChild(titulo);
-    item.appendChild(imagen);
-    item.appendChild(descripcion);
-    
     return item;
 }
--- componentes/seccion1/seccion1.js
+++ componentes/seccion1/seccion1.js
@@ -7,10 +7,10 @@ export function seccion1(){
 
     
 let flores = [
-    ["---", "https://img.freepik.com/fotos-premium/2408-35-um-close-detalhado-de-uma-flor-de-peonia-azul-colocada-contra-um-fundo-branco-pristino-as-petalas-em-camadas-lisas-irradiam-do-centro-exibindo-ricos-tons-de-azul-em-forte-contraste-com-o_1292484-26286.jpg"],
-    ["---", "https://png.pngtree.com/png-vector/20240313/ourmid/pngtree-white-peony-flower-watercolor-style-for-decorative-element-png-image_11944398.png"],
-    ["---", "https://www.pngplay.com/wp-content/uploads/10/Peony-PNG-Images-HD.png"],
-    ["---", "https://static.vecteezy.com/system/resources/previews/024/190/498/non_2x/beautiful-red-common-peony-free-png.png"]
+    ["HOLA 1", "https://img.freepik.com/fotos-premium/2408-35-um-close-detalhado-de-uma-flor-de-peonia-azul-colocada-contra-um-fundo-branco-pristino-as-petalas-em-camadas-lisas-irradiam-do-centro-exibindo-ricos-tons-de-azul-em-forte-contraste-com-o_1292484-26286.jpg"],
+    ["HOLA 2", "https://png.pngtree.com/png-vector/20240313/ourmid/pngtree-white-peony-flower-watercolor-style-for-decorative-element-png-image_11944398.png"],
+    ["HOLA 3", "https://www.pngplay.com/wp-content/uploads/10/Peony-PNG-Images-HD.png"],
+    ["HOLA 4", "https://static.vecteezy.com/system/resources/previews/024/190/498/non_2x/beautiful-red-common-peony-free-png.png"]
 ];
 
 // Para añadirlas a la sección
