ec72eb403daeea37fb4c0db64856e32bb89d8076
cami <pcbarrondo@gmail.com>
2025-08-19 09:26:04 +0200

    creacion de los productos

--- componentes/item/itemModulo.js
+++ componentes/item/itemModulo.js
@@ -1,4 +1,4 @@
-export function item(titulo, urlImg){
+export function item(titulo, urlImg, description, price){
     let item = document.createElement("div");
     item.className = "item";
     item.textContent = titulo;
@@ -7,10 +7,25 @@ export function item(titulo, urlImg){
     img.src = urlImg;
     item.appendChild(img);
 
+    let h3 = document.createElement('h3');
+    h3.textContent = titulo;
+    item.appendChild(h3);
+
+    let p = document.createElement('p');
+    p.textContent = description;
+    item.appendChild(p);
+
+    let span = document.createElement('span');
+    span.textContent = "$" + price;
+    item.appendChild(span);
+
+    
+
     item.addEventListener("click",()=>{
         item.classList.add("verde");
         let header = document.querySelector(".header");
         header.classList.add('ocultar');
     });
+    //funcion anonima: ()=>{}
     return item;
 }
--- componentes/seccion1/seccion1.js
+++ componentes/seccion1/seccion1.js
@@ -1,22 +1,17 @@
 import { item } from "../item/itemModulo.js";
+import { productos } from "../../database/productos.js";
 
 export function seccion1(){
+
     let seccion = document.createElement("seccion");
     seccion.className = "seccion-1"
 
 
-    
-let flores = [
-    ["HOLA 1", "https://img.freepik.com/fotos-premium/2408-35-um-close-detalhado-de-uma-flor-de-peonia-azul-colocada-contra-um-fundo-branco-pristino-as-petalas-em-camadas-lisas-irradiam-do-centro-exibindo-ricos-tons-de-azul-em-forte-contraste-com-o_1292484-26286.jpg"],
-    ["HOLA 2", "https://png.pngtree.com/png-vector/20240313/ourmid/pngtree-white-peony-flower-watercolor-style-for-decorative-element-png-image_11944398.png"],
-    ["HOLA 3", "https://www.pngplay.com/wp-content/uploads/10/Peony-PNG-Images-HD.png"],
-    ["HOLA 4", "https://static.vecteezy.com/system/resources/previews/024/190/498/non_2x/beautiful-red-common-peony-free-png.png"]
-];
+    // Para añadirlas a la sección
+    productos.forEach(Element => {
+       seccion.appendChild(item(Element.title,Element.image,Element.description,Element.price));
+    });
 
-// Para añadirlas a la sección
-flores.forEach(flor => {
-    seccion.appendChild(item(flor[0], flor[1]));
-});
 
     return seccion;
-}
+    }
